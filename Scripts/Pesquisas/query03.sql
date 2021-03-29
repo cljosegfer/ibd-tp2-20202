fc48d6a2692e651011b27751914eefab50ce0c23
jose <macaverde604@gmail.com>
2021-03-29 18:50:52 -0300

    corrige pesquisa 3 e 4

--- Scripts/Pesquisas/query03.sql
+++ Scripts/Pesquisas/query03.sql
@@ -1,1 +1,2 @@
+# Nome de todos os pacientes que se vacinaram com CoronaVac
 SELECT P.NOME FROM PACIENTE AS P, VACINA AS V WHERE P.NOME_VACINA = V.NOME AND V.NOME = 'CoronaVac';
--- Scripts/Pesquisas/query04.sql
+++ Scripts/Pesquisas/query04.sql
@@ -1,1 +1,2 @@
+# CPF de todos os pacientes que se vacinaram em São Paulo
 SELECT P.CPF FROM PACIENTE AS P, UNIDADE_VACINACAO AS U WHERE P.ID_POSTO = U.ID AND (U.ID = 'SP001' OR U.ID = 'SP002' OR U.ID = 'SP003');
