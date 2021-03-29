e959840540dcbbad627e4c0592d4ffecacdaba67
jose <macaverde604@gmail.com>
2021-03-29 19:09:13 -0300

    adiciona pesquisa 5

---
+++ Scripts/Pesquisas/query05.sql
@@ -0,0 +1,2 @@
+# Nome de vacinas que necessitam de refrigeração especial e têm mais de dez fornecedores
+SELECT V.NOME, COUNT(*) AS QUANTIDADE FROM VACINA AS V, FORNECEDOR AS F WHERE V.NOME = F.NOME_VACINA AND V.REFRIGERAÇAO = 1 GROUP BY V.NOME HAVING COUNT(*) > 10;
